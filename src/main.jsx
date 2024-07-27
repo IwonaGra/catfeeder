225e909474aaa02a7e12b4f172e1d94eebfd8e34
IwonaGra <iwogra@vp.pl>
2024-07-27 10:15:24 +0200

    Added: imports, rootElement validation, render of structure of components to DOM, Redux store integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App"; // tu łączę Main z App
+import { theme } from "./theme"; // tu importuję theme z lokalnego pliku
+import { store } from "./store"; // tu łączę się ze store
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+	throw new Error("Root element not found"); // dodana walidacja - sprawdzam po ID czy element root został dodany (index.html)
+}
+
+ReactDOM.createRoot(rootElement).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Suspense fallback={<div>Loading...</div>}>
+					<App />
+				</Suspense>
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>
 );
+
+// ostylować jeszcze Loading - kiepsko wygląda ... CircularProgress z Mui będzie pasować
